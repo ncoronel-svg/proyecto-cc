2cf061355febfa51d008b5cbe33dff2ccd68cb06
Nahuel Alexander Coronel <90536623+Nahuxc@users.noreply.github.com>
2026-03-24 19:30:00 -0300

    update aritmetica

--- aritmetica/aritmetica.py
+++ aritmetica/aritmetica.py
@@ -1,1 +1,27 @@
 #hacer libreria de calculos suma, resta, division, multiplicacion, modulo
+
+
+def suma(a,b):
+    resultado = a + b
+    return resultado
+
+print(suma(2,3))
+
+def resta(a,b):
+    resultado = a - b
+    return resultado
+
+print(resta(3,2))
+
+def multi(a, b):
+    resultado = a * b
+    return resultado
+
+print(multi(3,2))
+
+def div(a, b):
+    resultado = a / b
+    return resultado
+
+print(div(2,3))
+
