5bfd53c7e850401f490d97bc61f0514eb970d414
benja <benjaminbenavides308@gmail.com>
2026-03-24 20:22:09 -0300

    user update

--- aritmetica/aritmetica.py
+++ aritmetica/aritmetica.py
@@ -5,23 +5,24 @@ def suma(a,b):
     resultado = a + b
     return resultado
 
-print(suma(2,3))
+
 
 def resta(a,b):
     resultado = a - b
     return resultado
 
-print(resta(3,2))
+
 
 def multi(a, b):
     resultado = a * b
     return resultado
 
-print(multi(3,2))
+
 
 def div(a, b):
     resultado = a / b
     return resultado
 
-print(div(2,3))
+def modulo(a):
+    pass
 
