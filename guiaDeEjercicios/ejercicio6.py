2abfc33bd3c08160dba8ce9e2d4599d64617679d
nahuel alexander coronel <ncoronel@frba.utn.edu.ar>
2026-04-09 18:53:58 -0300

    Update ejercicio6.py

--- guiaDeEjercicios/ejercicio6.py
+++ guiaDeEjercicios/ejercicio6.py
@@ -1,8 +1,19 @@
 #Pedir un número y decir si es mayor a 10.
 
+# ESTO ESTA BIEN
 num1 = int(input("ingrese un numero: "))
 
+# ESTO ESTA MAL, NO PEDIMOS FUNCIONES Y DE TODAS FORMAS PASA a COMO PARAMETRO ESTA MAL TAMBIEN, HAY QUE SER MAS
+# PROLIJO USAR numero , como parametro, pensar bien los parametros a usar
+
+# DE IGUAL FORMA REHACERLO SIN FUNCION
+
+
+
+
 def mayordiez(a):
+
+    #ESTAS CONDICIONES MUY BIEN, VERIFICA LOS 3 POSIBLES CASOS, MUY BIEN
     if a == 10:
         print("el numero es igual a 10 .-.")
     elif a > 10:
