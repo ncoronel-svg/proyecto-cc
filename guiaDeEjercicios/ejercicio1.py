9bc58e83b3fd1170c05472cb4e5a32ab51351c5e
nahuel alexander coronel <ncoronel@frba.utn.edu.ar>
2026-04-09 18:41:12 -0300

    Refactor variable names and improve comments
    Correct variable naming and add comments for clarity.

--- guiaDeEjercicios/ejercicio1.py
+++ guiaDeEjercicios/ejercicio1.py
@@ -1,10 +1,26 @@
 # Crear variables para nombre, edad y altura. Mostrar todo en una sola línea.
 
+# ######## ESTA MAL ################
+
+# HAY QUE TENER EN CUENTA QUE CUANDO REFIERE A CREAR VARIABLES, VOS YA ESTAS ALMACENANDO Y CREANDO UNA VARIABLE PARA EL NOMBRE
+
+# NO VEO RAZON DE CREAR nombre , edad, ya con nombre1 y edad1 esta bien
+
+#FALTA COMENTAR # NOMBRE  , # EDAD , #ALTURA
+
+#MAL
 nombre1 = input("ingrese un nombre: ")
 nombre = nombre1
+
+#MAL
 edad1 = int(input("ingrese su edad: "))
 edad = edad1
+
+#BIEN
 altura = float(input("ingrese su altura"))
+
+
+# BORRARLO Y CORREGIR, TAMBIEN BORRAR MIS COMENTARIOS LUEGO DE LA CORRECION
 
 #print(f" nombre: {nombre} , edad: {edad}, altura : {altura}")
 
